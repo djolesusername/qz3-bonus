405b3bd5c468cfb6775bc2a249216ecb6b4de98b
Djordje Cvetkovic <djole001@gmail.com>
2021-09-29 08:07:57 +0200

    js basic validation

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-console.log('1')
-
 const $firstname = document.getElementById('first-name')
 const $lastname = document.getElementById('last-name')
 const $email = document.getElementById('email')
 const $password = document.getElementById('psw')
+const $form = document.getElementById("sign-up-form")
 
 //const $error = document.getElementById('error-message')
 
-function toggleVisibility  ($firstname,$lastname,  $email, $password )  {
-    $firstname.preventDefault()
-    console.log($firstname)
+const errorMessagePrefix = {
+ "psw" : "Password ",
+"first-name": "First Name ",
+"last-name": "Last Name",
+"email": "Email "
 
-  
+}
+
+function onInvalidForm  (e )  {
+    let target_error = e.target.nextElementSibling
+    e.preventDefault()
+
+    console.log(e)
+
+//since we are checking only email validity at the moment only thing that has data and can be invalid is email
+if (e.originalTarget.value && e.target.id === "email") {
+    target_error.innerHTML = "Looks like this is not an email"
+    target_error.classList.add("hasError");
+
+    return
+  }
+
+//Empty required fields check up 
+if (!e.originalTarget.value) {
+    let errorMessage = `${errorMessagePrefix[e.target.id]} cannot be empty `
+    target_error.innerHTML = errorMessage
+    target_error.classList.add("hasError");
+}
+
 };
 
-$firstname.addEventListener('invalid', toggleVisibility);
-$lastname.addEventListener('invalid', toggleVisibility);
-$email.addEventListener('invalid', toggleVisibility);
-$password.addEventListener('invalid', toggleVisibility);
+function clearErrors(e) {
+    e.originalTarget.nextElementSibling.classList.remove("hasError")
+
+}
+
+
+$firstname.addEventListener("input", clearErrors)
+$lastname.addEventListener("input", clearErrors)
+$password.addEventListener("input", clearErrors)
+$email.addEventListener("input", clearErrors)
+
+$firstname.addEventListener('invalid', onInvalidForm);
+$lastname.addEventListener('invalid', onInvalidForm);
+$email.addEventListener('invalid', onInvalidForm);
+$password.addEventListener('invalid', onInvalidForm);
+
