d96dbf0235b873349eb6360fb7b95a234cd44962
Djordje <djole001@gmail.com>
2021-11-09 14:31:08 +0100

    linting

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
-const $firstname = document.getElementById('first-name')
-const $lastname = document.getElementById('last-name')
-const $email = document.getElementById('email')
-const $password = document.getElementById('psw')
-const $form = document.getElementById("sign-up-form")
+const $firstname = document.getElementById("first-name");
+const $lastname = document.getElementById("last-name");
+const $email = document.getElementById("email");
+const $password = document.getElementById("psw");
+const $form = document.getElementById("sign-up-form");
 
 //const $error = document.getElementById('error-message')
 
 const errorMessagePrefix = {
- "psw" : "Password ",
-"first-name": "First Name ",
-"last-name": "Last Name",
-"email": "Email "
-
-}
-
-function onInvalidForm  (e )  {
-    let target_error = e.target.nextElementSibling
-    e.preventDefault()
-
-    console.log(e)
-
-//since we are checking only email validity at the moment only thing that has data and can be invalid is email
-if (e.originalTarget?.value && e.target.id === "email") {
-    target_error.innerHTML = "Looks like this is not an email"
-    target_error.classList.add("hasError");
-    e.target.classList.add("inputHasError")
-
-    return
-  }
-
-//Empty required fields check up 
-if (!e.originalTarget?.value) {
-    let errorMessage = `${errorMessagePrefix[e.target.id]} cannot be empty `
-    target_error.innerHTML = errorMessage
-    target_error.classList.add("hasError");
-    e.target.classList.add("inputHasError")
-
-}
-
+  psw: "Password ",
+  "first-name": "First Name ",
+  "last-name": "Last Name",
+  email: "Email ",
 };
 
-function clearErrors(e) {
-    e.originalTarget?.nextElementSibling.classList.remove("hasError")
-    e.target.classList.remove("inputHasError")
+function onInvalidForm(e) {
+  let target_error = e.target.nextElementSibling;
+  e.preventDefault();
 
+  console.log(e);
 
+  //since we are checking only email validity at the moment only thing that has data and can be invalid is email
+  if (e.originalTarget?.value && e.target.id === "email") {
+    target_error.innerHTML = "Looks like this is not an email";
+    target_error.classList.add("hasError");
+    e.target.classList.add("inputHasError");
+
+    return;
+  }
+
+  //Empty required fields check up
+  if (!e.originalTarget?.value) {
+    let errorMessage = `${errorMessagePrefix[e.target.id]} cannot be empty `;
+    target_error.innerHTML = errorMessage;
+    target_error.classList.add("hasError");
+    e.target.classList.add("inputHasError");
+  }
 }
 
+function clearErrors(e) {
+  e.originalTarget?.nextElementSibling.classList.remove("hasError");
+  e.target.classList.remove("inputHasError");
+}
 
-$firstname.addEventListener("input", clearErrors)
-$lastname.addEventListener("input", clearErrors)
-$password.addEventListener("input", clearErrors)
-$email.addEventListener("input", clearErrors)
+$firstname.addEventListener("input", clearErrors);
+$lastname.addEventListener("input", clearErrors);
+$password.addEventListener("input", clearErrors);
+$email.addEventListener("input", clearErrors);
 
-$firstname.addEventListener('invalid', onInvalidForm);
-$lastname.addEventListener('invalid', onInvalidForm);
-$email.addEventListener('invalid', onInvalidForm);
-$password.addEventListener('invalid', onInvalidForm);
-
+$firstname.addEventListener("invalid", onInvalidForm);
+$lastname.addEventListener("invalid", onInvalidForm);
+$email.addEventListener("invalid", onInvalidForm);
+$password.addEventListener("invalid", onInvalidForm);
